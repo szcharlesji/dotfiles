eca51a8ed90d2b9beb544e5f575122660e04d4cd
Charles Ji <me@charlesji.com>
2026-06-01 15:49:40 -0400

    config neovide

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -20,3 +20,29 @@ vim.filetype.add({
 
 -- Set root directory to cwd
 vim.g.root_spec = { "cwd" }
+
+if vim.g.neovide then
+  vim.o.guifont = "TX-02:h15"
+
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_padding_top = 8
+  vim.g.neovide_padding_bottom = 8
+  vim.g.neovide_padding_left = 8
+  vim.g.neovide_padding_right = 8
+
+  -- vim.g.neovide_window_blurred = true -- macOS only
+  -- vim.g.neovide_floating_blur_amount_x = 2.0
+  -- vim.g.neovide_floating_blur_amount_y = 2.0
+  -- vim.g.neovide_opacity = 0.05
+
+  vim.g.neovide_cursor_animation_length = 0.03
+  vim.g.neovide_cursor_trail_size = 0.3
+  vim.g.neovide_cursor_vfx_mode = "pixiedust" -- try "railgun" or "pixiedust" if you like fireworks
+
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_refresh_rate = 120 -- match your display
+
+  -- macOS: make Cmd work as a real modifier
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_input_use_logo = true
+end
