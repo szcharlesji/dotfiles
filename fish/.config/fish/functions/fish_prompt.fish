57682db3ec383882acc52f065cc4614639105f6a
Charles Ji <charlesji@trashcan.polecat-ionian.ts.net>
2026-07-23 17:20:05 +0000

    add hostname to fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function fish_prompt
     set -l dir (basename (prompt_pwd))
     set -l branch (git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null)
     set_color green
-    echo -n "$dir"
+    echo -n "$dir"@(hostname)
     set_color normal
     if test -n "$branch"
         echo -n " ($branch)"
