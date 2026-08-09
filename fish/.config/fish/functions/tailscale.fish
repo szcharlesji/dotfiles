873cf939f266b694b4bbcb53a77c8ef4fbefdf6f
Charles Ji <me@charlesji.com>
2026-08-09 23:23:00 +0000

    macos/linux flag for tailscale

--- fish/.config/fish/functions/tailscale.fish
+++ fish/.config/fish/functions/tailscale.fish
@@ -1,3 +1,9 @@
-function tailscale
-    /Applications/Tailscale.app/Contents/MacOS/Tailscale $argv
-end
+  function tailscale
+      set -l macos_tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale
+
+      if test -x $macos_tailscale
+          $macos_tailscale $argv
+      else
+          command tailscale $argv
+      end
+  end
